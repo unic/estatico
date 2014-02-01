2c3bbbf5a44fdb59bacac417015d88bab6b291d8
Thomas Jaggi <thomas.jaggi@gmail.com>
2014-02-02 00:42:10 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,165 @@
+// Load plugins
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	swig = swig = require('gulp-swig'),
+	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	clean = require('gulp-clean'),
+	livereload = require('gulp-livereload'),
+	lr = require('tiny-lr'),
+	server = lr(),
+	connect = require('connect'),
+	http = require('http'),
+	open = require('open'),
+	modernizr = require('gulp-modernizr'),
+	exec = require('child_process').exec;
+
+
+var config = {
+		sass: {
+			style: gutil.env.production ? 'compressed' : 'expanded',
+			loadPath: ['source/assets/vendor']
+		},
+		autoprefixer: 'last 2 version',
+		paths: {
+			jshint: [
+				'./source/assets/js/*.js',
+				'!./source/assets/vendor/*.js',
+				'!./source/assets/js/.tmp/*.js'
+			],
+			head: [
+				'./source/assets/js/.tmp/modernizr.js',
+				'./source/assets/js/head.js'
+			],
+			main: [
+				'./source/assets/vendor/jquery/jquery.js',
+				'./source/assets/js/.tmp/lodash.js',
+				'./source/assets/js/*.js',
+				'!./source/assets/js/.tmp/modernizr.js',
+				'!./source/assets/js/head.js'
+			],
+			modernizr: [
+				'./source/assets/js/*.js',
+				'./source/assets/css/*.scss',
+				'!./source/assets/js/.tmp/modernizr.js'
+			],
+			lodash: 'source/assets/js/.tmp/lodash.js'
+		},
+		modernizr: {
+			// excludeTests: ['supports']
+		},
+		lodash: {
+			include: ['template', 'each', 'debounce']
+		},
+		swig: {
+			ext: '.html',
+			defaults: {
+				cache: false
+			},
+			data: {
+				text: 'Welcome'
+			}
+		}
+	};
+
+
+// HTML
+gulp.task('html', function() {
+	return gulp.src('./source/*.html')
+		.pipe(swig(config.swig))
+		.pipe(gulp.dest('./build'))
+		.pipe(livereload(server));
+});
+
+// Styles
+gulp.task('css', function() {
+	return gulp.src('./source/assets/css/main.scss')
+		.pipe(sass(config.sass))
+		.pipe(autoprefixer(config.autoprefixer))
+		.pipe(gulp.dest('./build/assets/css'))
+		.pipe(livereload(server));
+});
+
+// Scripts
+gulp.task('js', function() {
+	gulp.src(config.paths.jshint)
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('jshint-stylish'));
+
+	gulp.src(config.paths.head)
+		.pipe(concat('head.js'))
+		.pipe(gutil.env.production ? uglify() : gutil.noop())
+		.pipe(gulp.dest('./build/assets/js'))
+		.pipe(livereload(server));
+
+	gulp.src(config.paths.main)
+		.pipe(concat('main.js'))
+		.pipe(gutil.env.production ? uglify() : gutil.noop())
+		.pipe(gulp.dest('./build/assets/js'))
+		.pipe(livereload(server));
+});
+
+gulp.task('modernizr', function() {
+	return gulp.src(config.paths.modernizr)
+		.pipe(modernizr(config.modernizr))
+		.pipe(gutil.env.production ? uglify() : gutil.noop())
+		.pipe(gulp.dest('./source/assets/js/.tmp'));
+});
+
+gulp.task('lodash', function() {
+	exec('lodash include=' + config.lodash.include.join(',') + ' -o ' + config.paths.lodash + ' -d');
+});
+
+// Clean
+gulp.task('clean', function() {
+	return gulp.src(['build'], {
+			read: false
+		})
+		.pipe(clean());
+});
+
+// Watch
+gulp.task('watch', function() {
+	// Listen on port 35729
+	server.listen(35729, function (err) {
+		if (err) {
+			return console.log(err)
+		};
+
+		gulp.watch('source/{,*/}*.html', ['html']);
+		gulp.watch('source/assets/css/*.scss', ['css']);
+		gulp.watch('source/assets/js/*.js', ['js']);
+	});
+});
+
+// Connect server
+gulp.task('server', ['html', 'css', 'js', 'watch'], function() {
+	var app = connect()
+			.use(require('connect-livereload')())
+			.use(connect.static('build')),
+		server = http.createServer(app).listen(9000);
+
+	server.on('listening', function() {
+		open('http://localhost:9000');
+	});
+
+	// Clean on exit
+	process.on('SIGINT', function () {
+		exec('gulp clean', function () {
+			process.exit(0);
+		});
+	});
+});
+
+// Build
+gulp.task('build', ['clean'], function() {
+	gulp.start('html', 'css', 'js');
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+	gulp.start('server');
+});
