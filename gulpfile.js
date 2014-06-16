26d4d063cd55abe9ee8428cd621daed070264525
Thomas Jaggi <thomas.jaggi@gmail.com>
2014-06-16 21:40:26 +0200

    Fix lodash and modernizr tasks

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var gulp = require('gulp'),
 	_ = require('lodash'),
 	exec = require('child_process').exec,
 	runSequence = require('run-sequence'),
+	path = require('path'),
+	fs = require('fs'),
 
 	// Handlebars
 	handlebars = require('handlebars'),
@@ -157,24 +159,32 @@ gulp.task('modernizr', function() {
 		.pipe(gulp.dest('./source/assets/.tmp'));
 });
 
-// Use wrapper to prevent the slightly buggy modernizr task from "breaking the chain"
-gulp.task('modernizr-wrapper', function(cb) {
-	exec('node_modules/.bin/gulp modernizr', cb)
-});
-
 /**
  * Generate customized lodash build in source/assets/.tmp/
  */
 gulp.task('lodash', function(cb) {
-	var modules = ['debounce'],
+	var cmdDir = 'node_modules/.bin/',
+		targetDir = 'source/assets/.tmp/',
+		targetFile = 'lodash.js',
+		relTargetPath = path.relative(cmdDir, targetDir + targetFile),
+		modules = ['debounce'],
 		args = [
 			'include=' + modules.join(','),
 			'-o',
-			'../../source/assets/.tmp/lodash.js',
+			relTargetPath,
 			'-d'
 		];
 
-	exec('cd node_modules/.bin/ && lodash ' + args.join(' '), cb);
+	// Create source/assets/.tmp directory if not already present
+	if (!fs.existsSync(targetDir)) {
+		fs.mkdirSync(targetDir, function(err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+	}
+
+	exec('cd ' + cmdDir + ' && lodash ' + args.join(' '), cb);
 });
 
 /**
@@ -303,7 +313,8 @@ gulp.task('watch', function() {
  * Create build directory
  */
 gulp.task('build', function(callback) {
-	runSequence('clean', ['lodash', 'modernizr-wrapper', 'iconfont', 'pngsprite'], ['html', 'css', 'jshint', 'js-head', 'js-main', 'media'], callback);
+	// Currently, the modernizr task cannot run in parallel with other tasks. This should get fixed as soon as Modernizr 3 is published and the plugin is officially released.
+	runSequence('clean', ['lodash', 'iconfont', 'pngsprite'], 'modernizr', ['html', 'css', 'jshint', 'js-head', 'js-main', 'media'], callback);
 });
 
 /**
