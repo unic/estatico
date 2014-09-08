dfe8d0a5b2dac2f866ef5195c3642783ba1e5ac6
Oriol Torrent Florensa <oriol.tf@me.com>
2014-09-08 17:00:01 +0200

    REmoved TODO

--- gulp/build.js
+++ gulp/build.js
@@ -8,7 +8,6 @@ var gulp = require('gulp'),
 	runSequence = require('run-sequence');
 
 gulp.task('build', function (cb) {
-	// TODO: is this true? Needs review.
 	// Currently, the modernizr task cannot run in parallel with other tasks. This should get fixed as soon as Modernizr 3 is published and the plugin is officially released.
 	runSequence('clean', ['js:lodash', 'media:iconfont', 'media:pngsprite'], 'js:modernizr', ['html', 'css', 'js:hint', 'js:head', 'js:main', 'media:copy'], function (err) {
 		if (err) {
