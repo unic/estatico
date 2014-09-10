0dded3c355e47a61e38f89f049673e226b7fb027
Thomas Jaggi <thomas.jaggi@gmail.com>
2014-09-10 10:53:07 +0200

    Remove empty lines

--- handlebars/index.js
+++ handlebars/index.js
@@ -7,13 +7,10 @@ var handlebars = require('handlebars');
 require('handlebars-layouts')(handlebars);
 
 
-
-
 // Custom Handlebars Helper
 
 // Remove whitespace from string
 handlebars.registerHelper('removeWhiteSpace', function(value) {
-
 	return new handlebars.SafeString(
 		value.replace(/\s+/g, '')
 	);
