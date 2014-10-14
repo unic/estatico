8653982e234aa09c3db309f195ecf5cc1b536087
Thomas Jaggi <thomas.jaggi@gmail.com>
2014-10-14 18:27:44 +0200

    Handlebars: Add dynamic partials and demo of comparions

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -7,8 +7,13 @@ var handlebars = require('handlebars');
 require('handlebars-layouts')(handlebars);
 
 // Make specific assemble helpers available
+// See http://assemble.io/helpers/ for a documentation
+//
 // Example: Use the comparisons provided by the handlebars-helpers package
-// require('../node_modules/handlebars-helpers/lib/helpers/helpers-comparisons.js').register(handlebars);
+require('../node_modules/handlebars-helpers/lib/helpers/helpers-comparisons.js').register(handlebars);
+//
+// WARNING: For some helpers, grunt has to be installed (npm install grunt --save && npm shrinkwrap)
+// This might be fixed at some point: https://github.com/assemble/handlebars-helpers/pull/157
 
 
 // Custom Handlebars Helper
@@ -23,4 +28,19 @@ handlebars.registerHelper('removeWhiteSpace', function(value) {
 // Output raw block (use: {{{{raw}}}} blabla {{title}} bla{{{{/raw}}}})
 handlebars.registerHelper('raw', function(options) {
   return options.fn();
+});
+
+// Include partial with dynamic name
+// Based on http://stackoverflow.com/a/21411521
+handlebars.registerHelper('dynamicPartial', function(name, context) {
+	var template = handlebars.partials[name],
+		output;
+
+	if (typeof template !== 'function') {
+		template = handlebars.compile(template);
+	}
+
+	output = template(context).replace(/^\s+/, '');
+
+	return new handlebars.SafeString(output);
 });
