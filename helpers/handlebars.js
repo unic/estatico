baf48f053e1940aae00db3b34bb0bfad849916ef
Thomas Jaggi <thomas.jaggi@gmail.com>
2014-10-15 09:47:51 +0200

    Add placeholder capability for dynamic partial name

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,7 +1,9 @@
 'use strict';
 
 // Handlebars
-var handlebars = require('handlebars');
+var handlebars = require('handlebars'),
+	errors = require('./errors.js'),
+	_ = require('lodash');
 
 // Make handlebars layout helpers available
 require('handlebars-layouts')(handlebars);
@@ -32,15 +34,38 @@ handlebars.registerHelper('raw', function(options) {
 
 // Include partial with dynamic name
 // Based on http://stackoverflow.com/a/21411521
-handlebars.registerHelper('dynamicPartial', function(name, context) {
-	var template = handlebars.partials[name],
+// @param {String} name - Partial path, can contain placeholder as "{{key}}" where "key" is a property of the context data
+// @param {Object} data - Data to pass to compile function
+handlebars.registerHelper('dynamicPartial', function(name, data, context) {
+	var placeholders = name.match(/{{(.*?)}}/g),
+		template,
 		output;
+
+	_.each(placeholders, function(placeholder) {
+		var key = placeholder.replace(/({|})/g, ''),
+			value = data[key] || (context ? context.data.root[key] : data.data.root[key]);
+
+		if (value) {
+			name = name.replace(placeholder, value);
+		}
+	});
+
+	template = handlebars.partials[name];
+
+	if (template === undefined) {
+		errors({
+			task: 'helpers/handlebars',
+			message: 'Dynamic template "'+ name +'" not found'
+		});
+
+		return '';
+	}
 
 	if (typeof template !== 'function') {
 		template = handlebars.compile(template);
 	}
 
-	output = template(context).replace(/^\s+/, '');
+	output = template(data || context).replace(/^\s+/, '');
 
 	return new handlebars.SafeString(output);
 });
