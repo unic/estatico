beed9b803c76ceddc57eae5fc182c7873c16b21c
Thomas Jaggi <thomas.jaggi@gmail.com>
2014-12-26 21:11:41 +0100

    FROBOIL-63: Write productive QUnit test for slideshow

--- source/modules/slideshow/slideshow.test.js
+++ source/modules/slideshow/slideshow.test.js
@@ -1,6 +1,25 @@
-/* jshint eqeqeq:false */
 'use strict';
 
-test('Test non-strict equality', function() {
-	ok(1 == '1', 'Check whether 1 equals "1"');
+QUnit.test('Test whether plugin instance was saved on DOM element', function(assert) {
+	var $slideshow = $('.mod_slideshow'),
+		instance = $slideshow.data('plugin_slideshow');
+
+	assert.ok(typeof instance === 'object', 'Plugin instance is an object');
+});
+
+QUnit.test('Test whether nav buttons were added', function(assert) {
+	var $slideshow = $('.mod_slideshow'),
+		$buttons = $slideshow.find('button[data-slideshow]');
+
+	assert.ok($buttons.length === 2, 'Two buttons found');
+});
+
+QUnit.test('Test whether clicking "prev" button updates currentItem property', function(assert) {
+	var $slideshow = $('.mod_slideshow'),
+		$button = $slideshow.find('button.next'),
+		instance = $slideshow.data('plugin_slideshow');
+
+	$button.trigger('click');
+
+	assert.ok(instance.currentItem === 1, 'currentItem is 1');
 });
