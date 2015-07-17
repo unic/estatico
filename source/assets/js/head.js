c4fd875e8b0f40a68a236e526ecebd5b90f96e60
David Simon <me@davidsimon.ch>
2015-07-17 12:39:18 +0200

    [Fix] Log: use custom bind implementation to satisfy quit since phantom <2.0 does not have Function.prototype.bind

--- source/assets/js/head.js
+++ source/assets/js/head.js
@@ -15,6 +15,26 @@
 
 		// Helper methods
 		helpers: {
+		 	// PhantomJS doesn't support bind yet
+			// This serves as a fill-in taken from es5.js
+			// https://github.com/inexorabletash/polyfill
+			bind: function (o) {
+				if (typeof this !== 'function') {
+					throw new TypeError('Bind must be called on a function');
+				}
+				var slice = [].slice,
+					args = slice.call(arguments, 1),
+					self = this,
+				bound = function() {
+					return self.apply(this instanceof NOP ? this : o, args.concat(slice.call(arguments)));
+				};
+
+				function NOP() {}
+				NOP.prototype = self.prototype;
+				bound.prototype = new NOP();
+
+				return bound;
+			},
 			// Deep extend (before $.extend is available)
 			extend: function extend(destination, source) {
 				for (var property in source) {
@@ -30,13 +50,13 @@
 				return destination;
 			},
 			// Creates a console.log wrapper with optional namespace/context
-			log: function log(context) {
+			log: function log(contextName) {
 				var _fn;
 
-				if (typeof context === 'string' && context.length > 0) {
-					_fn = Function.prototype.bind.call(console.log, console, context);
+				if (typeof contextName === 'string' && contextName.length > 0) {
+					_fn = estatico.helpers.bind.call(console.log, console, contextName);
 				} else {
-					_fn = Function.prototype.bind.call(console.log, console);
+					_fn = estatico.helpers.bind.call(console.log, console);
 				}
 
 				return _fn;
