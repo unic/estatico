b1ccb83f1c080ce54aca567269c26807d4f871e6
Thomas Jaggi <thomas.jaggi@unic.com>
2015-12-07 12:14:07 +0100

    ESTATICO-191, ESTATICO-192: Formatting

--- source/assets/js/helpers/aria.js
+++ source/assets/js/helpers/aria.js
@@ -1,165 +1,137 @@
-/*
+/**
+ * Aria helper, outlines elements with aria attributes
+ *
+ * Start the debugging with ctrl+a (same to switch to next mode)
+ */
 
-Aria Helper
------------
+;(function(undefined) {
+	'use strict';
 
-Start the debugging with ctrl+a (same to switch to next mode)
+	estatico.helpers.aria = {
+		mode: null,
+		dataAttribute: 'bookmarkletlog',
 
-*/
+		// Add some css to the document so we visually see which elemnt has focus
+		init: function() {
+			var mainColor = 'rgba(0,100,255,0.2)',
+				activeColor = 'rgba(255,0,0,0.2)',
+				mainBorderStyle = 'inset 0 0 0 4px '+mainColor,
+				activeBorderStyle = 'inset 0 0 0 4px '+activeColor,
+				newCSS,
+				tag;
 
-'use strict';
+			// Create the styles
+			newCSS = '.aria-debugging-bookmarklet{position:relative !important;box-shadow: '+mainBorderStyle+' !important;-webkit-box-shadow: '+mainBorderStyle+' !important;-moz-box-shadow: '+mainBorderStyle+' !important}';
+			newCSS += '.aria-debugging-bookmarklet::after{z-index:9999999 !important;position:absolute !important;top:-15px !important;left:0 !important;white-space:nowrap !important;background:'+mainColor+' !important;color:#000 !important;font-size:10px !important;content:attr(data-'+this.dataAttribute+') !important}';
+			newCSS += '.aria-debugging-active-bookmarklet{z-index:9999999 !important;box-shadow: '+activeBorderStyle+' !important;-webkit-box-shadow: '+activeBorderStyle+' !important;-moz-box-shadow: '+activeBorderStyle+' !important}';
 
-estatico.helpers.aria = {
+			// Add all new styles to the document
+			if ('\v' === 'v') {
+				document.createStyleSheet().cssText = newCSS;
+			} else {
+				tag = document.createElement('style');
+				tag.type = 'text/css';
+				document.getElementsByTagName('head')[0].appendChild(tag);
+				tag[ (typeof document.body.style.WebkitAppearance === 'string') ? 'innerText' : 'innerHTML' ] = newCSS;
+			}
+		},
 
-	mode: null,
-	dataAttribute: 'bookmarkletlog',
+		run: function() {
+			// Set the mode we're in (1 = active element, 2 = all aria elements)
+			if (this.mode === null) {
+				this.mode = 1;
 
-	init: function(){
+				this.init();
+			} else {
+				this.mode++;
+			}
 
-		// Add some css to the document so we visually see which elemnt has focus:
+			// Run the current mode
+			if (this.mode === 1) {
+				this.addActiveElement();
+			} else if (this.mode === 2) {
+				this.removeActiveElement();
 
-		var mainColor = 'rgba(0,100,255,0.2)',
-			activeColor = 'rgba(255,0,0,0.2)',
-			mainBorderStyle = 'inset 0 0 0 4px '+mainColor,
-			activeBorderStyle = 'inset 0 0 0 4px '+activeColor,
-			newCSS,
-			tag;
+				this.addClassToAriaElements();
+			} else {
+				this.removeClassFromAriaElements();
+			}
+		},
 
-		// Create the styles:
-		
-		newCSS = '.aria-debugging-bookmarklet{position:relative !important;box-shadow: '+mainBorderStyle+' !important;-webkit-box-shadow: '+mainBorderStyle+' !important;-moz-box-shadow: '+mainBorderStyle+' !important}';
-		newCSS += '.aria-debugging-bookmarklet::after{z-index:9999999 !important;position:absolute !important;top:-15px !important;left:0 !important;white-space:nowrap !important;background:'+mainColor+' !important;color:#000 !important;font-size:10px !important;content:attr(data-'+this.dataAttribute+') !important}';
-		newCSS += '.aria-debugging-active-bookmarklet{z-index:9999999 !important;box-shadow: '+activeBorderStyle+' !important;-webkit-box-shadow: '+activeBorderStyle+' !important;-moz-box-shadow: '+activeBorderStyle+' !important}';
+		// Add class to the active element
+		addActiveElement: function() {
+			var activeEl = null;
 
-		// Add all new styles to the document:
+			window._activeElInterval = setInterval(function() {
+				window._currentActiveEl = document.activeElement;
 
-		if ('\v' === 'v'){
-			document.createStyleSheet().cssText = newCSS;
-		} else {
-			tag = document.createElement('style');
-			tag.type = 'text/css';
-			document.getElementsByTagName('head')[0].appendChild(tag);
-			tag[ (typeof document.body.style.WebkitAppearance === 'string') ? 'innerText' : 'innerHTML' ] = newCSS;
-		}
+				if (window._currentActiveEl !== activeEl) {
+					if (activeEl !== null) {
+						activeEl.classList.remove('aria-debugging-active-bookmarklet');
+					}
 
-	},
+					activeEl = window._currentActiveEl;
 
-	run: function(){
+					console.log(activeEl);
 
-		// Set the mode we're in (1 = active element, 2 = all aria elements):
+					window._currentActiveEl.classList.add('aria-debugging-active-bookmarklet');
+				}
+			}, 200);
+		},
 
-		if (this.mode === null){
-			this.mode = 1;
-			this.init();
-		} else {
-			this.mode++;
-		}
+		// Remove active element
+		removeActiveElement: function() {
+			clearInterval(window._activeElInterval);
 
-		// Run the current mode:
+			window._currentActiveEl.classList.remove('aria-debugging-active-bookmarklet');
 
-		if (this.mode === 1){
-			
-			this.addActiveElement();
+			delete window._activeElInterval;
+			delete window._currentActiveEl;
 
-		} else if (this.mode === 2) {
+		},
 
-			this.removeActiveElement();
+		// Add class to all aria elements
+		addClassToAriaElements: function() {
+			var nodeList,
+				log,
+				i,
+				j;
 
-			this.addClassToAriaElements();
-			
-		} else {
+			nodeList = document.getElementsByTagName('*');
 
-			this.removeClassFromAriaElements();
+			for (i = 0; i < nodeList.length; i++) {
+				log = '';
 
-		}
-
-	},
-
-	addActiveElement: function(){
-
-		// Add class to the active element: 
-
-		var activeEl = null;
-
-		window._activeElInterval = setInterval(function() {
-
-			window._currentActiveEl = document.activeElement;
-
-			if (window._currentActiveEl !== activeEl) {
-
-				if (activeEl !== null){
-					activeEl.classList.remove('aria-debugging-active-bookmarklet');
+				for (j = nodeList[i].attributes.length - 1; j >= 0; j--) {
+					if (nodeList[i].attributes[j].name === 'role' || nodeList[i].attributes[j].name.substring(0, 5) === 'aria-') {
+						log += '['+nodeList[i].attributes[j].name+'='+nodeList[i].attributes[j].value+']';
+					}
 				}
 
-				activeEl = window._currentActiveEl;
-				console.log(activeEl);
+				if (log !== '') {
+					console.log(nodeList[i], log);
 
-				window._currentActiveEl.classList.add('aria-debugging-active-bookmarklet');
-
-			}
-
-		}, 200);
-
-	},
-
-	removeActiveElement: function(){
-
-		// Remove active element:
-
-		clearInterval(window._activeElInterval);
-		window._currentActiveEl.classList.remove('aria-debugging-active-bookmarklet');
-
-		delete window._activeElInterval;
-		delete window._currentActiveEl;
-
-	},
-
-	addClassToAriaElements: function(){
-
-		var nodeList,
-			log,
-			i,
-			j;
-
-		// Add class to all aria elements:
-		
-		nodeList = document.getElementsByTagName('*');
-
-		for (i = 0; i < nodeList.length; i++) {
-
-			log = '';
-
-			for (j = nodeList[i].attributes.length - 1; j >= 0; j--){
-				if (nodeList[i].attributes[j].name === 'role' || nodeList[i].attributes[j].name.substring(0, 5) === 'aria-'){
-					log += '['+nodeList[i].attributes[j].name+'='+nodeList[i].attributes[j].value+']';
+					nodeList[i].classList.add('aria-debugging-bookmarklet');
+					nodeList[i].dataset[this.dataAttribute] = log;
 				}
+
 			}
 
-			if (log !== ''){
-				console.log(nodeList[i], log);
-				nodeList[i].classList.add('aria-debugging-bookmarklet');
-				nodeList[i].dataset[this.dataAttribute] = log;
+		},
+
+		// Remove class from aria elements
+		removeClassFromAriaElements: function() {
+			var nodeList,
+				i;
+
+			nodeList = document.getElementsByTagName('*');
+
+			for (i = 0; i < nodeList.length; i++) {
+				nodeList[i].classList.remove('aria-debugging-bookmarklet');
 			}
 
+			this.mode = 0;
 		}
+	};
 
-	},
-
-	removeClassFromAriaElements: function(){
-
-		var nodeList,
-			i;
-
-		// Remove aria elements:
-
-		nodeList = document.getElementsByTagName('*');
-
-		for (i = 0; i < nodeList.length; i++) {
-			nodeList[i].classList.remove('aria-debugging-bookmarklet');
-		}
-
-		this.mode = 0;
-
-	}
-	
-};
+})();
--- source/assets/js/helpers/inspector.js
+++ source/assets/js/helpers/inspector.js
@@ -1,132 +1,115 @@
-/*
+/**
+ * Module inspector, outlines Estatico modules
+ *
+ * Start inspection with ctrl+m (same to switch off module inspection)
+ */
 
-Module Inspector
-----------------
+;(function(undefined) {
+	'use strict';
 
-Start inspection with ctrl+m (same to switch off module inspection)
+	estatico.helpers.inspector = {
+		mode: null,
+		dataAttribute: 'bookmarkletlog',
 
-*/
+		// Add some css to the document so we visually see which elemnt has focus
+		init: function() {
+			var mainColor = 'rgba(100,200,255,0.4)',
+				mainBorderStyle = 'inset 0 0 0 4px '+mainColor,
+				newCSS,
+				tag;
 
-'use strict';
+			// Create the styles
+			newCSS = '.estatico-bookmarklet{position:relative !important;box-shadow: '+mainBorderStyle+' !important;-webkit-box-shadow: '+mainBorderStyle+' !important;-moz-box-shadow: '+mainBorderStyle+' !important}';
+			newCSS += '.estatico-bookmarklet::after{text-transform:capitalize !important;z-index:9999999 !important;position:absolute !important;top:-15px !important;left:0 !important;white-space:nowrap !important;background:'+mainColor+' !important;color:#000 !important;font-size:10px !important;content:attr(data-'+this.dataAttribute+') !important}';
 
-estatico.helpers.inspector = {
+			// Add all new styles to the document
+			if ('\v' === 'v') {
+				document.createStyleSheet().cssText = newCSS;
+			} else {
+				tag = document.createElement('style');
+				tag.type = 'text/css';
 
-	mode: null,
-	dataAttribute: 'bookmarkletlog',
+				document.getElementsByTagName('head')[0].appendChild(tag);
 
-	init: function(){
+				tag[(typeof document.body.style.WebkitAppearance === 'string') ? 'innerText' : 'innerHTML'] = newCSS;
+			}
 
-		// Add some css to the document so we visually see which elemnt has focus:
+		},
 
-		var mainColor = 'rgba(100,200,255,0.4)',
-			mainBorderStyle = 'inset 0 0 0 4px '+mainColor,
-			newCSS,
-			tag;
+		run: function() {
+			// Set the mode we're in (1 = show modules, 0 = hide modules)
+			if (this.mode === null) {
+				this.mode = 1;
 
-		// Create the styles:
-		
-		newCSS = '.estatico-bookmarklet{position:relative !important;box-shadow: '+mainBorderStyle+' !important;-webkit-box-shadow: '+mainBorderStyle+' !important;-moz-box-shadow: '+mainBorderStyle+' !important}';
-		newCSS += '.estatico-bookmarklet::after{text-transform:capitalize !important;z-index:9999999 !important;position:absolute !important;top:-15px !important;left:0 !important;white-space:nowrap !important;background:'+mainColor+' !important;color:#000 !important;font-size:10px !important;content:attr(data-'+this.dataAttribute+') !important}';
+				this.init();
+			} else {
+				this.mode++;
+			}
 
-		// Add all new styles to the document:
+			// Run the current mode
+			if (this.mode === 1) {
+				this.showModules();
+			} else {
+				this.hideModules();
+			}
+		},
 
-		if ('\v' === 'v'){
-			document.createStyleSheet().cssText = newCSS;
-		} else {
-			tag = document.createElement('style');
-			tag.type = 'text/css';
-			document.getElementsByTagName('head')[0].appendChild(tag);
-			tag[ (typeof document.body.style.WebkitAppearance === 'string') ? 'innerText' : 'innerHTML' ] = newCSS;
-		}
+		// Add class to all modules
+		showModules: function() {
+			var nodeList,
+				log,
+				module,
+				variations,
+				i,
+				j;
 
-	},
+			nodeList = document.getElementsByTagName('*');
 
-	run: function(){
+			for (i = 0; i < nodeList.length; i++) {
+				log = '';
+				module = '';
+				variations = [];
 
-		// Set the mode we're in (1 = active element, 2 = all aria elements):
+				for (j = nodeList[i].classList.length - 1; j >= 0; j--) {
+					if (nodeList[i].classList[j].substring(0, 4) === 'mod_') {
+						module = nodeList[i].classList[j].substring(4).replace(/_/g, ' ');
+					}
 
-		if (this.mode === null){
-			this.mode = 1;
-			this.init();
-		} else {
-			this.mode++;
-		}
-
-		// Run the current mode:
-
-		if (this.mode === 1){
-			
-			this.showModules();
-
-		} else {
-
-			this.hideModules();
-
-		}
-
-	},
-
-	showModules: function(){
-
-		var nodeList,
-			log,
-			module,
-			variations,
-			i,
-			j;
-
-		// Add class to all aria elements:
-		
-		nodeList = document.getElementsByTagName('*');
-
-		for (i = 0; i < nodeList.length; i++) {
-
-			log = '';
-			module = '';
-			variations = [];
-
-			for (j = nodeList[i].classList.length - 1; j >= 0; j--){
-				if (nodeList[i].classList[j].substring(0, 4) === 'mod_'){
-					module = nodeList[i].classList[j].substring(4).replace(/_/g, ' ');
+					if (nodeList[i].classList[j].substring(0, 4) === 'var_') {
+						variations.push( nodeList[i].classList[j].substring(4).replace(/_/g, ' ') );
+					}
 				}
-				if (nodeList[i].classList[j].substring(0, 4) === 'var_'){
-					variations.push( nodeList[i].classList[j].substring(4).replace(/_/g, ' ') );
+
+				if (module !== '') {
+					log = module;
+				}
+
+				if (variations.length > 0) {
+					log += ': ' + variations.join(', ');
+				}
+
+				if (log !== '') {
+					console.log(nodeList[i], log);
+
+					nodeList[i].classList.add('estatico-bookmarklet');
+					nodeList[i].dataset[this.dataAttribute] = log;
 				}
 			}
+		},
 
-			if (module !== ''){
-				log = module;
+		// Remove class from modules
+		hideModules: function() {
+			var nodeList,
+				i;
+
+			nodeList = document.getElementsByTagName('*');
+
+			for (i = 0; i < nodeList.length; i++) {
+				nodeList[i].classList.remove('estatico-bookmarklet');
 			}
 
-			if (variations.length > 0){
-				log += ': ' + variations.join(', ');
-			}
-
-			if (log !== ''){
-				console.log(nodeList[i], log);
-				nodeList[i].classList.add('estatico-bookmarklet');
-				nodeList[i].dataset[this.dataAttribute] = log;
-			}
-
+			this.mode = 0;
 		}
+	};
 
-	},
-
-	hideModules: function(){
-
-		var nodeList,
-			i;
-
-		// Remove aria elements:
-
-		nodeList = document.getElementsByTagName('*');
-
-		for (i = 0; i < nodeList.length; i++) {
-			nodeList[i].classList.remove('estatico-bookmarklet');
-		}
-
-		this.mode = 0;
-
-	}
-	
-};
+})();
