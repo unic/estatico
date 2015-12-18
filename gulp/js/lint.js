b86acbfca240563056a2af998a6f489abf81ae3f
Oriol Torrent <oriol.torrent@unic.com>
2015-12-18 17:09:34 +0100

    ESTATICO-153: Added JSCS linting to lint task

--- gulp/js/lint.js
+++ gulp/js/lint.js
@@ -24,17 +24,31 @@ gulp.task(taskName, function() {
 		tap = require('gulp-tap'),
 		path = require('path'),
 		cached = require('gulp-cached'),
-		jshint = require('gulp-jshint');
+		jshint = require('gulp-jshint'),
+		jscs = require('gulp-jscs');
 
 	return gulp.src(taskConfig.src)
 		.pipe(cached('linting'))
 		.pipe(jshint())
+		.pipe(jscs({
+			configPath: '.jscsrc',
+			fix: true
+		}))
 		.pipe(jshint.reporter('jshint-stylish'))
 		.pipe(util.env.dev ? tap(function(file) {
 			if (!file.jshint.success) {
 				helpers.errors({
 					task: taskName,
 					message: 'Linting error in file "' + path.relative('./source/', file.path) + '" (see console)'
+				});
+			}
+			if (!file.jscs.success) {
+				file.jscs.errors.reverse();
+				file.jscs.errors.forEach(function(error, index) {
+					helpers.errors({
+						task: 'js:jscs',
+						message: error.message + ' (rule: ' + error.rule + ') in file ' + path.relative('./source/', file.path) + ':' + error.line + ':' + error.column + ' (see console)'
+					});
 				});
 			}
 		}) : jshint.reporter('fail').on('error', helpers.errors));
