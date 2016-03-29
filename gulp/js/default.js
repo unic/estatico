23f54ec8505eaa884d54de72587533a6b73b95a0
Oriol Torrent <oriol.torrent@unic.com>
2016-03-29 19:14:59 +0200

    ESTATICO-188: add webpack config to parse only JSX files with the react preset

--- gulp/js/default.js
+++ gulp/js/default.js
@@ -110,7 +110,15 @@ var taskName = 'js',
 										exclude: /node_modules/,
 										loader: 'babel-loader',
 										query: {
-											presets: ['es2015', 'stage-0', 'react']
+											presets: ['es2015', 'stage-0']
+										}
+									},
+									{
+										test: /(\.jsx)$/,
+										exclude: /node_modules/,
+										loader: 'babel-loader',
+										query: {
+											presets: ['react']
 										}
 									}
 								]
