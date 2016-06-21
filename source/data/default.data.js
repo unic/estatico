0305ff89ac810e8b2258ba62ea96be01baa41cf7
Marta Pieczynska <marta.pieczynska@unic.com>
2016-06-21 14:14:07 +0200

    ESTATICO-212 Add SVG sprite demo module

--- source/data/default.data.js
+++ source/data/default.data.js
@@ -3,7 +3,10 @@
 var util = require('gulp-util'),
 	data = {
 		project: 'Estático',
-		env: util.env
+		env: util.env,
+		svgSprites: JSON.stringify([
+			'/assets/media/svg/base.svg'
+		])
 	};
 
 module.exports = data;
---
+++ source/demo/modules/svgsprite/svgsprite.md
@@ -0,0 +1,7 @@
+## Description
+
+
+
+## Integration
+
+Copy HTML.
