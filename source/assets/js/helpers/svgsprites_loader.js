8d7d72c52b92032b6573a65717186a6ff73e354d
Marta Pieczynska <marta.pieczynska@unic.com>
2016-07-28 14:34:35 +0200

    ESTATICO-212: Refactor svgsprite_helper and update to newest version from SBB

--- source/assets/js/helpers/svgsprites_loader.js
+++ source/assets/js/helpers/svgsprites_loader.js
@@ -13,85 +13,97 @@ var loadSvgSprites = function() {
 
 	var id = 'mod_svgsprites',
 		oReq,
-		spritesToLoad = JSON.parse(document.body.getAttribute('data-svgsprites-options')) || [],
-		spritesAmount = spritesToLoad.length,
+		spritesToLoad,
+		spritesAmount,
 		spriteContainer = document.createElement('div');
 
-	// If sprite was already added, do not execute further
-	if (document.getElementById(id) !== null) {
-		return false;
-	}
+	setTimeout(function() {
 
-	/**
-	 * Check if we can send a XMLHttpRequest
-	 * @returns {*}
-	 */
-	function getXMLHttpRequest() {
-		if (window.XMLHttpRequest) {
-			return new window.XMLHttpRequest();
-		} else {
-			try {
-				return new ActiveXObject('MSXML2.XMLHTTP.3.0');
-			} catch (e) {
-				return null;
-			}
-		}
-	}
+		spritesToLoad = JSON.parse(document.body.getAttribute('data-svgsprites-options'));
+		spritesAmount = spritesToLoad.length;
 
-	/**
-	 * RequestSVG
-	 * @param url -> string holding the url to the svg file
-	 * @constructor
-	 */
-	function RequestSVG(url) {
-		var oReq = getXMLHttpRequest(),
-			$container = document.getElementById(id),
-			handler = function() {
-				if (oReq.readyState === 4) { // complete
-					if (oReq.status === 200) {
-						$container.innerHTML = $container.innerHTML + oReq.responseText;
-					}
+		/**
+		 * Check if we can send a XMLHttpRequest
+		 * @returns {*}
+		 */
+		function getXMLHttpRequest() {
+			if (window.XMLHttpRequest) {
+				return new window.XMLHttpRequest();
+			} else {
+				try {
+					return new ActiveXObject('MSXML2.XMLHTTP.3.0');
+				} catch (e) {
+					return null;
 				}
-			};
-
-		oReq.open('GET', url, true);
-		oReq.onreadystatechange = handler;
-		oReq.send();
-	}
-
-	/**
-	 * Send getXMLHttpRequest for each SVG sprite reference
-	 * found in the data-icon-sets attribute on the body tag
-	 */
-	if (spritesAmount > 0 && (document.getElementById(id) === null)) {
-		var i = spritesAmount,
-			html = document.getElementsByTagName('html')[0];
-
-		oReq = getXMLHttpRequest();
-
-		if (oReq !== null) {
-			spriteContainer.setAttribute('id', id);
-			spriteContainer.setAttribute('data-svgsprites', 'wrapper'); // for potential later usage within JavaScript
-			spriteContainer.setAttribute('style', 'display: none');
-
-			document.body.appendChild(spriteContainer);
-			while (i--) {
-				var request = new RequestSVG(spritesToLoad[i]); // jshint ignore:line
 			}
-
-			if (window.requestAnimationFrame) {
-				window.requestAnimationFrame(function() {
-					html.setAttribute('class', html.getAttribute('class') + ' svgSpritesLoaded');
-				});
-			}
-
 		}
-	}
+
+		/**
+		 * RequestSVG
+		 * @param url - string holding the url to the svg file
+		 * @constructor
+		 */
+		function RequestSVG(url) {
+			var oReq = getXMLHttpRequest(),
+				$container = document.getElementById(id),
+				handler = function() {
+					if (oReq.readyState === 4) { // complete
+						if (oReq.status === 200) {
+							$container.innerHTML = $container.innerHTML + oReq.responseText;
+						}
+					}
+				};
+
+			oReq.open('GET', url, true);
+			oReq.onreadystatechange = handler;
+			oReq.send();
+		}
+
+		/**
+		 * Send getXMLHttpRequest for each SVG sprite reference
+		 * found in the data-icon-sets attribute on the body tag
+		 */
+		if (spritesAmount > 0 && (document.getElementById(id) === null)) {
+			var i = spritesAmount,
+				html = document.getElementsByTagName('html')[0],
+				request;
+
+			oReq = getXMLHttpRequest();
+
+			if (oReq !== null) {
+				spriteContainer.setAttribute('id', id);
+				spriteContainer.setAttribute('data-svgsprites', 'wrapper'); // for potential later usage within JavaScript
+				spriteContainer.setAttribute('style', 'display: none');
+
+				document.body.appendChild(spriteContainer);
+				while (i--) {
+					request = new RequestSVG(spritesToLoad[i]);
+				}
+
+				if (window.requestAnimationFrame) {
+					window.requestAnimationFrame(function() {
+						html.setAttribute('class', html.getAttribute('class') + ' svgSpritesLoaded');
+					});
+				} else {
+					html.setAttribute('class', html.getAttribute('class') + ' svgSpritesLoaded');
+				}
+
+			}
+		}
+
+	}, 100);
+
 };
 
 /**
- * In some rare cases DOMContentLoaded already happend before we end up here.
- * To make sure that the icons are getting loaded we add the second call as well
+ * Load the SVG Sprite
  */
-document.addEventListener('DOMContentLoaded', loadSvgSprites);
-loadSvgSprites();
+
+document.addEventListener('DOMContentLoaded', loadSvgSprites());
+
+// Alternative to DOMContentLoaded event
+/* document.onreadystatechange = function() {
+	if (document.readyState === 'complete') {
+		loadSvgSprites();
+	}
+} */
