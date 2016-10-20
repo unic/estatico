f4f388a72a5933fa1d7f4878eac798a3d7f07ce2
Thomas Jaggi <thomas.jaggi@unic.com>
2016-10-20 18:08:56 +0200

    ESTATICO-212: Standardize file naming, import loader

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,6 +1,7 @@
 import datasetPolyfill from 'element-dataset';
 import '../../../node_modules/handlebars/dist/handlebars';
 import './helpers/module';
+import './helpers/svgspriteloader';
 
 import EstaticoApp from './helpers/estaticoapp';
 
