32ad302369997d7105629d9c636e9a6dd72ba02e
Sebastian Weyrauch <git@dev-lab.de>
2017-01-17 09:57:39 +0100

    fixed and improved JS scaffolding

--- source/modules/.scaffold/scaffold.js
+++ source/modules/.scaffold/scaffold.js
@@ -1,39 +1,49 @@
-/*!
+import EstaticoModule from '../../assets/js/helpers/module';
+
+const name = '{{name}}';
+
+/*
  * {{originalName}}
  *
  * @author
  * @copyright
  */
-import EstaticoModule from '../../assets/js/helpers/module';
-
-class {{className}} extends EstaticoModule {
+export default class {{className}} extends EstaticoModule {
+	static events = {
+		// eventname: `eventname.estatico.${name}`
+	};
+	static defaultData = {};
+	static defaultOptions = {
+		domSelectors: {
+			// item: `[data-${name}="item"]`
+		},
+		stateClasses: {
+			// activated: 'is_activated'
+		}
+	};
 
 	constructor($element, data, options) {
-		let _defaultData = {},
-			_name = '{{name}}',
-			_defaultOptions = {
-				domSelectors: {
-					// item: '[data-' + _name + '="item"]'
-				},
-				stateClasses: {
-					// activated: 'is_activated'
-				}
-			};
-
-		super($element, _defaultData, _defaultOptions, data, options);
+		super($element, {{className}}.defaultData, {{className}}.defaultOptions, data, options);
 
 		this._initUi();
 		this._initEventListeners();
 	}
 
-	static get events() {
-		return {
-			// eventname: 'eventname.estatico.' + {{name}}
-		};
+	/**
+	 * Unbind events, remove data, custom teardown
+	 *
+	 * @public
+	 */
+	destroy() {
+		super.destroy();
+
+		// Custom destroy actions go here
 	}
 
 	/**
 	 * Initialisation of variables, which point to DOM elements
+	 *
+	 * @private
 	 */
 	_initUi() {
 		// DOM element pointers
@@ -41,19 +51,10 @@ class {{className}} extends EstaticoModule {
 
 	/**
 	 * Event listeners initialisation
+	 *
+	 * @private
 	 */
 	_initEventListeners() {
 		// Event listeners
 	}
-
-	/**
-	 * Unbind events, remove data, custom teardown
-	 */
-	destroy() {
-		super.destroy();
-
-		// Custom destroy actions go here
-	}
 }
-
-export default {{className}};
