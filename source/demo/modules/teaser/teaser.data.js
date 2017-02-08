e2dcd1150f5a994fbced431328dbd192c38d8588
swey <git@dev-lab.de>
2017-02-08 14:18:03 +0100

    Update teaser.data.js

--- source/demo/modules/teaser/teaser.data.js
+++ source/demo/modules/teaser/teaser.data.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var _ = require('lodash'),
-		requireNew = require('require-new'),
-		dataHelper = require('../../../../helpers/data.js'),
-		defaultData = requireNew('../../../data/default.data.js');
+	requireNew = require('require-new'),
+	dataHelper = require('../../../../helpers/data.js'),
+	defaultData = requireNew('../../../data/default.data.js');
 
 var data = _.merge(defaultData, {
 		meta: {
