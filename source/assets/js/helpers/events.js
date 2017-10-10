c05a3753542f09574f37f8dac0c1134693fd2c28
Oriol Torrent Florensa <oriol.tf@me.com>
2017-10-10 09:38:05 +0200

    Add option to register throttled events
    This is specially needed for scroll events

--- source/assets/js/helpers/events.js
+++ source/assets/js/helpers/events.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import debounce from 'lodash/debounce';
+import throttle from 'lodash/throttle';
 
 /**
  * Adds debounced global resize and scroll events and generates public methods for adding handlers
@@ -22,15 +23,18 @@ class WindowEventListener {
 
 		let events = {
 			resize: {
-				interval: 50
+				interval: 50,
+				throttleInterval: 10
 			},
 			scroll: {
-				interval: 50
+				interval: 50,
+				throttleInterval: 10
 			}
 		};
 
 		for (let eventName of Object.keys(events)) {
 			this._registerDebouncedEvent(eventName, events[eventName]);
+			this._registerThrottledEvent(eventName, events[eventName]);
 		}
 	}
 
@@ -51,10 +55,28 @@ class WindowEventListener {
 
 		// adds a public shorthand method, e.g. addResizeListener to the WindowEventListener class
 		this[`addDebounced${methodName}Listener`] = this._addEventListener.bind(this, debouncedEventName);
+		this[`removeDebounced${methodName}Listener`] = this._removeEventListener.bind(this, debouncedEventName);
 
 		// Save to global namespace
 		$.extend(true, estatico, { events: {} });
 		estatico.events[eventName] = debouncedEventName;
+	}
+
+	_registerThrottledEvent(eventName, config) {
+		let throttledEventName = `throttled${eventName}.estatico`,
+			methodName = eventName.charAt(0).toUpperCase() + eventName.slice(1);
+
+		this.$window.on(eventName, throttle(function(event) {
+			$(document).triggerHandler(throttledEventName, event);
+		}.bind(this), config.throttleInterval));
+
+		// adds a public shorthand method, e.g. addResizeListener to the WindowEventListener class
+		this[`addThrottled${methodName}Listener`] = this._addEventListener.bind(this, throttledEventName);
+		this[`removeThrottled${methodName}Listener`] = this._removeEventListener.bind(this, throttledEventName);
+
+		// Save to global namespace
+		$.extend(true, estatico, { events: {} });
+		estatico.events[eventName] = throttledEventName;
 	}
 
 	/**
@@ -71,6 +93,14 @@ class WindowEventListener {
 		}
 
 		$(document).on(eventName, callback);
+	}
+
+	_removeEventListener(eventName, uuid) {
+		if (uuid) {
+			eventName = eventName + '.' + uuid;
+		}
+
+		$(document).off(eventName);
 	}
 }
 
